511cf36085a37d0a1544ad796114ce937edfeb7c
Zoe Tretiakova <trettttiakpva@gmail.com>
2021-11-28 22:54:51 +0300

    initial. T2 done

---
+++ hw0/src/HW0/T2.hs
@@ -0,0 +1,13 @@
+module T2 where
+import Data.Void
+
+type Not a = a -> Void
+
+doubleNeg :: a -> Not (Not a)  
+-- a -> (a -> Void) -> Void
+doubleNeg a f = f a
+
+reduceTripleNeg :: Not (Not (Not a)) -> Not a
+-- (((a -> Void) -> Void) -> Void) -> a -> Void
+reduceTripleNeg f a = f (doubleNeg a)
+
