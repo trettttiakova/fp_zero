7229f78ddd7999b702d0a695f0f0d851b13f99ab
Zoe Tretiakova <trettttiakpva@gmail.com>
2021-11-28 23:57:10 +0300

    T1 done

--- hw0/src/HW0/T1.hs
+++ hw0/src/HW0/T1.hs
@@ -14,5 +14,18 @@ distrib :: Either a (b, c) -> (Either a b, Either a c)
 distrib (Left a)       = (Left a, Left a)
 distrib (Right (b, c)) = (Right b, Right c)
 
--- assocPair :: (a, (b, c)) <-> ((a, b), c)
+assocPair :: (a, (b, c)) <-> ((a, b), c)
+assocPair = Iso (\(a, (b, c)) -> ((a, b), c)) (\((a, b), c) -> (a, (b, c)))
+
+assocEither :: Either a (Either b c) <-> Either (Either a b) c
+assocEither = Iso firstFunc secondFunc
+  where
+    firstFunc :: Either a (Either b c) -> Either (Either a b) c
+    firstFunc (Left a)          = Left $ Left a
+    firstFunc (Right (Left b))  = Left $ Right b
+    firstFunc (Right (Right c)) = Right c
+    secondFunc :: Either (Either a b) c -> Either a (Either b c)
+    secondFunc (Left (Left a)) = Left a
+    secondFunc (Left (Right b))  = Right $ Left b
+    secondFunc (Right c) = Right $ Right c
 
