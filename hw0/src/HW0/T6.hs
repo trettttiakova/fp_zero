d5452b5bd62c19d220d80915e7c2904b2aea005f
Zoe Tretiakova <trettttiakpva@gmail.com>
2021-11-29 14:50:59 +0300

    T6 partly done

---
+++ hw0/src/HW0/T6.hs
@@ -0,0 +1,13 @@
+module HW0.T6 where
+import HW0.T1 (distrib)
+import Data.Char (isSpace)
+
+a = distrib (Left ("AB" ++ "CD" ++ "EF"))
+b = map isSpace "Hello, World"
+c = if 1 > 0 || error "X" then "Y" else "Z"
+
+a_whnf = (Left ("AB" ++ "CD" ++ "EF"), Left ("AB" ++ "CD" ++ "EF"))
+
+b_whnf = (isSpace 'H') : (map isSpace "ello, World")
+
+c_whnf = "Y"
