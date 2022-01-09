f27628b04c42658ad215734e08d950f48eff7e82
Zoe Tretiakova <trettttiakpva@gmail.com>
2022-01-09 14:33:28 +0300

    done T5

--- hw0/src/HW0/T5.hs
+++ hw0/src/HW0/T5.hs
@@ -3,22 +3,26 @@ import GHC.Natural (Natural)
 
 type Nat a = (a -> a) -> a -> a
 
+-- Returns a Nut corresponding to zero.
 nz :: Nat a
 nz func x = x
 
+-- Returns an incremented Nat.
 ns :: Nat a -> Nat a
 -- ((a -> a) -> a -> a) -> (a -> a) -> a -> a
 ns nat func x = func $ nat func x
 
+-- Addition and multiplication for Nat.
 nplus, nmult :: Nat a -> Nat a -> Nat a
 -- ((a -> a) -> a -> a) -> ((a -> a) -> a -> a) -> (a -> a) -> a -> a
 nplus n1 n2 func x = n1 func $ n2 func x
-nmult n1 n2 func = n1 $ n2 func
+nmult n1 n2 func   = n1 $ n2 func
 
+-- Retuns a Nat corresponding to a given natural number.
 nFromNatural :: Natural -> Nat a
 nFromNatural 0 = nz
 nFromNatural n = ns $ nFromNatural $ n - 1
 
+-- Returns a natural number corresponding to a given Nat.
 nToNum :: Num a => Nat a -> a
-nToNum nz = 0
-
+nToNum nat = nat (+1) 0
